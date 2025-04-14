979f3914477aacc6624ad424248458cb73b188fc
brandonwirz <bwirz75@gmail.com>
2025-04-14 14:51:53 -0600

    chef claude app start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import Header from "./Header"
+import Main from "./MainForm"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
